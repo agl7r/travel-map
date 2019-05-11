a101dc7557e7232dde7397fd0c24db37482c2bca
Andrey Gladyshev <andrey@gladyshev.me>
2019-05-11 16:30:15 +0400

    Add first post.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,56 @@
-ymaps.ready(function () {
-  var map = new ymaps.Map('map', {
-    center: [55.751574, 37.573856],
-    zoom: 9,
-    controls: ['zoomControl']
+function fetchTravels() {
+  return fetch('data/travels.json')
+    .then(response => {
+      return response.json()
+    })
+}
+
+function createMap() {
+  return new Promise((resolve) => {
+    ymaps.ready(function () {
+      const map = new ymaps.Map('map', {
+        center: [0, 0],
+        zoom: 9,
+        controls: ['zoomControl']
+      })
+
+      const clusterer = new ymaps.Clusterer()
+      map.geoObjects.add(clusterer);
+
+      resolve({
+        add(object) {
+          clusterer.add(object)
+        },
+        updateBounds() {
+          map.setBounds(map.geoObjects.getBounds())
+        }
+      })
+    })
   })
-});
+}
+
+function renderPostOnMap(post, map) {
+  post.photos.forEach(photo => {
+    const placemark = new ymaps.Placemark([photo.coordinates.lng, photo.coordinates.lat])
+    map.add(placemark)
+  })
+}
+
+function renderTravelOnMap(travel, map) {
+  travel.posts.forEach((post) => {
+    renderPostOnMap(post, map)
+  })
+}
+
+async function run() {
+  const map = await createMap()
+  const travels = await fetchTravels()
+
+  travels.forEach(travel => {
+    renderTravelOnMap(travel, map)
+  })
+
+  map.updateBounds()
+}
+
+run()
